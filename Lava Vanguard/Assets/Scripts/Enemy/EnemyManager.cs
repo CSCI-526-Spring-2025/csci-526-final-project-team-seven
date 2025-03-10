a24195401b961fa6fa1bef4177dfc05c578a5cff
XZhou <a827414472@gmail.com>
2025-03-09 23:31:53 -0700

    Generate enemies according to the equipped modules

--- Lava Vanguard/Assets/Scripts/Enemy/EnemyManager.cs
+++ Lava Vanguard/Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,5 +1,9 @@
 using DG.Tweening;
 using UnityEngine;
+using Math = System.Math;
+using Random = UnityEngine.Random;
+using System.Collections;
+using Async;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -8,6 +12,13 @@ public class EnemyManager : MonoBehaviour
     public Transform enemyContainer;
     //[HideInInspector]
     public GameObject[] enemyPrefabs;
+
+    [Header("Enemy Settings")]
+    public float baseSpawnInterval = 3.0f;
+    public float minSpawnInterval = 0.3f;
+    public float maxSpawnInterval = 2f;
+    public float difficultyCurve = 0.3f;
+    private Coroutine spawnCoroutine;
     private void Awake()
     {
         Instance = this;
@@ -27,7 +38,40 @@ public class EnemyManager : MonoBehaviour
         return enemyView;
     }
     private void Start()
+    {   
+        StartSpawn();
+        
+        // InvokeRepeating(nameof(GenerateRandomEnemy), 1f, repeatRate);
+    }
+
+    public void StartSpawn()
+    {   
+        if (spawnCoroutine != null)
+        {
+            StopCoroutine(spawnCoroutine);
+        }
+        spawnCoroutine = StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
     {
-        InvokeRepeating(nameof(GenerateRandomEnemy), 1f, 1f);
+        yield return new WaitForSeconds(1f);
+
+        while (true)
+        {
+            GenerateRandomEnemy();
+            float waitTime = CalculateSpawnInterval();
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private float CalculateSpawnInterval()
+    {
+        int cardCount = SequenceManager.Instance.GetCardNumber();
+        int safeCardCount = Mathf.Max(cardCount, 1);
+        
+        float calculatedInterval = baseSpawnInterval / (Mathf.Log10(safeCardCount) * difficultyCurve);
+    
+        return Mathf.Clamp(calculatedInterval, minSpawnInterval, maxSpawnInterval);
     }
 }
