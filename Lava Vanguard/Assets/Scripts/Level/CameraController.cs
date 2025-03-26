af63914952440203b0275d96584f56ecd435cf02
CareEJoken <CareEJoken@gmail.com>
2025-03-26 12:45:00 -0700

    add cinemachine, prepare for perlin noise

--- Lava Vanguard/Assets/Scripts/Level/CameraController.cs
+++ Lava Vanguard/Assets/Scripts/Level/CameraController.cs
@@ -1,48 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
     public static CameraController Instance { get; private set; }
-    
+
     public GameObject player;
     public float cameraSpeedY = 0.3f;
     public float cameraFollowDistance = 5.0f;
 
-    private Camera mainCamera;
+    public CinemachineVirtualCamera virtualCamera;
     private bool moving = false;
+
     private void Awake()
     {
         Instance = this;
     }
+
     void Start()
     {
-        mainCamera = Camera.main;
         if (!Tutorial.Instance.tutorial)
             StartMove();
     }
+
     public void StartMove()
     {
         moving = true;
     }
+
     private void LateUpdate()
     {
         if (moving)
         {
-            Vector3 targetCameraPosition = mainCamera.transform.position;
+            Transform cameraTransform = virtualCamera.transform;
+            Vector3 targetPosition = cameraTransform.position;
 
-            targetCameraPosition.y += cameraSpeedY * Time.deltaTime;
-            if (player.transform.position.x > targetCameraPosition.x + cameraFollowDistance)
+            // 让相机沿着 Y 轴平滑上移
+            targetPosition.y += cameraSpeedY * Time.deltaTime;
+
+            // 根据玩家位置调整 X 轴的偏移
+            if (player.transform.position.x > targetPosition.x + cameraFollowDistance)
             {
-                targetCameraPosition.x = player.transform.position.x - cameraFollowDistance;
+                targetPosition.x = player.transform.position.x - cameraFollowDistance;
             }
-            else if (player.transform.position.x < targetCameraPosition.x - cameraFollowDistance)
+            else if (player.transform.position.x < targetPosition.x - cameraFollowDistance)
             {
-                targetCameraPosition.x = player.transform.position.x + cameraFollowDistance;
+                targetPosition.x = player.transform.position.x + cameraFollowDistance;
             }
-            mainCamera.transform.position = targetCameraPosition;
+
+            // 直接修改 Virtual Camera 的位置
+            cameraTransform.position = targetPosition;
         }
-
     }
 }
